d08fca188fb6e26b4ddaa66db2a0a28a1682096a
TejaGudivada47 <135949843+TejaGudivada47@users.noreply.github.com>
2023-08-15 11:22:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './Component/NavBar';
+import Home from './Component/Home';
+import AddProduct from './Component/AddProduct';
+import EditProduct from './Component/EditProduct';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/addProduct' element={<AddProduct />}></Route>
+        <Route path='/editProduct/:id' element={<EditProduct />}></Route>
+      </Routes>
+
+    </>
+
+  );
+}
+
+export default App;
